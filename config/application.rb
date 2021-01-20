18d068cffd1c4c9ebe8030cf505c53f4164b30fb
Mahad Hashi <adanhashimahad@gmail.com>
2021-01-20 12:40:41 +0900

    Sorted tasks/index in descending order of creation date

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,10 @@ Bundler.require(*Rails.groups)
 
 module Everyleaf
   class Application < Rails::Application
+    config.time_zone = 'GMT+3'
     config.load_defaults 5.2
     config.i18n.available_locales = [:en, :so]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :so
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
