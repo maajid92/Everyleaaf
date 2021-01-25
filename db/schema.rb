2df047c188ed761ad5e8111ce486f39123ee5a8a
Mahad Hashi <adanhashimahad@gmail.com>
2021-01-25 11:23:34 +0900

    Create a user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_18_032257) do
+ActiveRecord::Schema.define(version: 2021_01_25_020412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2021_01_18_032257) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "priority"
     t.date "Deadline"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
